6eae1b1e4a54d630b565efab3c504186cde3f94d
jdeng1040 <73567329+jdeng1040@users.noreply.github.com>
2023-11-18 20:33:29 -0500

    added math question

--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+import time
 
 # Initialize Pygame
 pygame.init()
@@ -7,37 +9,39 @@ pygame.init()
 # Set up the display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Input Menu")
+pygame.display.set_caption("Pygame Page Example")
 
 # Define colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 # Define fonts
-font = pygame.font.Font(None, 36)
-
-# Define input fields and button
-label1_rect = pygame.Rect(width // 4, height // 8 + 40, width // 2, 40)
-input_rect1 = pygame.Rect(width // 4, height // 4, width // 2, 40)
-label2_rect = pygame.Rect(width // 4, height // 2 - 50, width // 2, 40)
-input_rect2 = pygame.Rect(width // 4, height // 2 - 10, width // 2, 40)
-button_rect = pygame.Rect(width // 4, height * 3 // 4, width // 2, 40)
-
-# Input field values
-name = ""
-ip = ""
-
-# Define list of players
-player_list = [
-    "William",
-    "Oliver",
-    "Jason",
-]
+font_title = pygame.font.Font(None, 48)
+font_input = pygame.font.Font(None, 36)
 
 # Page identifiers
 PAGE_MAIN = "main"
-PAGE_RESULT = "result"
+PAGE_FAIL = "fail"
 current_page = PAGE_MAIN
+
+# Define input field properties
+input_rect = pygame.Rect(width // 4, height // 2, width // 2, 40)
+input_color_inactive = pygame.Color('lightskyblue3')
+input_color_active = pygame.Color('dodgerblue2')
+input_color = input_color_inactive
+input_text = ''
+input_active = False
+
+# Define button properties
+button_rect = pygame.Rect(width // 4, height // 2 + 60, width // 2, 40)
+button_color = pygame.Color('green')
+button_text = 'Enter'
+
+# Define the question
+# Generate a random integer between 1 and 10 (inclusive)
+first_num = random.randint(0, 12)
+second_num = random.randint(0, 12)
+question = str(first_num) + " * " + str(second_num)
 
 # Main game loop
 running = True
@@ -45,63 +49,58 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if input_rect1.collidepoint(pygame.mouse.get_pos()):
-                if event.key == pygame.K_BACKSPACE:
-                    name = name[:-1]
-                else:
-                    name += event.unicode
-            elif input_rect2.collidepoint(pygame.mouse.get_pos()):
-                if event.key == pygame.K_BACKSPACE:
-                    ip = ip[:-1]
-                else:
-                    ip += event.unicode
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1 and button_rect.collidepoint(pygame.mouse.get_pos()):
-                # Add code to transition to another page with input_text1 and input_text2 values
-                print(f"Transitioning to another page with inputs: {name}, {ip}")
-                # Button clicked, transition to another page
-                if current_page == PAGE_MAIN:
-                    current_page = PAGE_RESULT
-                elif current_page == PAGE_RESULT:
-                    current_page = PAGE_MAIN
+            if input_rect.collidepoint(event.pos):
+                input_active = not input_active
+                input_color = input_color_active if input_active else input_color_inactive
+            elif button_rect.collidepoint(event.pos):
+                if int(input_text) == first_num * second_num:
+                    print("GOOD")
+                else:
+                    current_page = PAGE_FAIL
 
-    # Draw the input fields and button
+        elif event.type == pygame.KEYDOWN:
+            if input_active:
+                if event.key == pygame.K_RETURN:
+                    if int(input_text) == first_num * second_num:
+                        print("GOOD")
+                    else:
+                        current_page = PAGE_FAIL
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+                else:
+                    input_text += event.unicode
+
+    # Draw the page
     screen.fill(white)
     if current_page == PAGE_MAIN:
-        label1_text = font.render("Enter your name:", True, black)
-        screen.blit(label1_text, (label1_rect.x + 5, label1_rect.y + 5))
+        # Draw title
+        title_surface = font_title.render(question, True, black)
+        title_rect = title_surface.get_rect(center=(width, height // 4))
+        screen.blit(title_surface, title_rect)
 
-        pygame.draw.rect(screen, black, input_rect1, 2)
-        text_surface = font.render(name, True, black)
-        screen.blit(text_surface, (input_rect1.x + 5, input_rect1.y + 5))
+        # Draw input field
+        pygame.draw.rect(screen, input_color, input_rect, 2)
+        input_surface = font_input.render(input_text, True, black)
+        width = max(input_rect.w, input_surface.get_width()+10)
+        input_rect.w = width
+        screen.blit(input_surface, (input_rect.x+5, input_rect.y+5))
 
-        label2_text = font.render("Enter server IP:", True, black)
-        screen.blit(label2_text, (label2_rect.x + 5, label2_rect.y + 5))
-
-        pygame.draw.rect(screen, black, input_rect2, 2)
-        text_surface = font.render(ip, True, black)
-        screen.blit(text_surface, (input_rect2.x + 5, input_rect2.y + 5))
-
-        pygame.draw.rect(screen, black, button_rect, 2)
-        button_text = font.render("JOIN GAME", True, black)
+        # Draw enter button
+        pygame.draw.rect(screen, button_color, button_rect)
+        button_surface = font_input.render(button_text, True, white)
         button_rect_center = button_rect.center
-        button_rect_center = (button_rect_center[0] - button_text.get_width() // 2, button_rect_center[1] - button_text.get_height() // 2)
-        screen.blit(button_text, button_rect_center)
-
-    elif current_page == PAGE_RESULT:
-        title_surface = font.render("Waiting for players...", True, black)
-        screen.blit(title_surface, (width // 2 - title_surface.get_width() // 2, height // 8))
-        for i, item in enumerate(player_list):
-            text_surface = font.render(str(i + 1) + ". " + item, True, black)
-            screen.blit(text_surface, (width // 2 - text_surface.get_width() // 2, height // 4 + i * 40))
-        # text_surface1 = font.render(name, True, black)
-        # screen.blit(text_surface1, (width // 2 - text_surface1.get_width() // 2, height // 4))
-
-        # # Render and blit the second phrase
-        # text_surface2 = font.render(ip, True, black)
-        # screen.blit(text_surface2, (width // 2 - text_surface2.get_width() // 2, height // 2))
-
+        button_rect_center = (button_rect_center[0] - button_surface.get_width() // 2, button_rect_center[1] - button_surface.get_height() // 2)
+        screen.blit(button_surface, button_rect_center)
+    elif current_page == PAGE_FAIL:
+        for i in range(5, 0, -1):
+            screen.fill(white)
+            text = font_input.render(str(i), True, black)
+            text_rect = text.get_rect(center=(width, height // 2))
+            screen.blit(text, text_rect)
+            pygame.display.flip()
+            time.sleep(1)
+        current_page = PAGE_MAIN
     pygame.display.flip()
 
 # Quit Pygame
